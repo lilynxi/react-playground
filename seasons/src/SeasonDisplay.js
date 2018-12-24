52d5e922176a55e71a935c483a9984b108db84ed
Lisa Müller <lisa.mueller47@gmail.com>
2018-12-24 12:29:04 +0100

    finalize season exercise

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import './SeasonDisplay.css';
 
-const SeasonDisplay = () => {
-  return(<div>asdf</div>)
+const seasonConfig = {
+  summer: {
+    text: 'summer',
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'winter',
+    iconName: 'snowflake',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9){
+    return lat > 0 ? 'summer': 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = ({lat}) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const {text, iconName} = seasonConfig[season]; // text, iconName
+  
+
+  return <div className={`season-display ${season}` }>
+    <h1>{text}</h1>
+    <i className={`${iconName} icon big`}></i>
+  </div>
 }
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props); // reference to the parents constructor function
+  // constructor(props){
+  //   super(props); // reference to the parents constructor function
 
-    this.state = { lat: null, errorMessage: false };
+  //   this.state = { lat: null, errorMessage: false };
+  // }
 
+  state = {lat: null, errorMessage: false};
+
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-        this.setState({lat: position.coords.latitude});
-      },
-      (err) => {
-        console.log(err);
-        this.setState({errorMessage: err.message})
-      }
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) => this.setState({errorMessage: err.message})
     );
   }
 
+  componentDidUpdate(){
+    console.log("Component did Update");
+  }
+
+  renderContent(){
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Err: {this.state.errorMessage}</div>;
+    }
+
+    if(!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat}/>;
+    }
+
+    return <Spinner text={"loading text"} />;
+  }
 
   render() {
-
-    //console.log(this.state.errorMessage);
-
-    return (
-      <div>
-        {
-          !this.state.errorMessage ?
-          <div>Latitude: {this.state.lat}</div>
-          :
-          <div>Err: {this.state.errorMessage}</div>
-        }
+    return(
+      <div className="blabla">
+        {this.renderContent()}
       </div>
-    )
+    );
 
   }
 }
